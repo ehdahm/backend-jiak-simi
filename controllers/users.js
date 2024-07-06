89703875d5b1997e98d453f01af2db6910321a0f
Adam Yee <adamyee96@gmail.com>
2024-07-06 19:09:30 +0800

    feat: add login ability, userSessions schema

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,8 @@ const UserModel = require("../models/users");
 
 module.exports = {
   signup,
+  getSaltAndIterations,
+  loginUser,
 };
 
 async function signup(req, res) {
@@ -11,5 +13,29 @@ async function signup(req, res) {
   } catch (err) {
     console.log(err);
     res.status(500).json({ err });
+  }
+}
+
+async function getSaltAndIterations(req, res) {
+  try {
+    const user = await UserModel.getSaltAndIterations(req.query);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const token = await UserModel.loginUser(req.body);
+    console.log(token);
+    if (!token.success) {
+      res.status(400).json({ errorMsg: token.error });
+      return;
+    }
+    res.json(token.data);
+  } catch (err) {
+    res.status(500).json({ errorMsg: err.message });
   }
 }
