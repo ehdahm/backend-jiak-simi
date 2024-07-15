83f8ff132355969dfdc22db894cba625a2b8cbf3
jolynntan <jolynntmy@gmail.com>
2024-07-16 02:29:55 +0800

    add updateReview functions

--- models/dishes.js
+++ models/dishes.js
@@ -9,49 +9,64 @@ module.exports = {
   findOrCreateDish,
   updatePrice,
   updateAvgRating,
-  fetchAllDishesIDs
+  fetchAllDishesIDs,
 };
 
 async function getDish(dish_id) {
-  console.log(`getDish model dish_id`, dish_id)
+  // console.log(`getDish model dish_id`, dish_id)
   let dishDoc = await daoDish.findOne({ _id: dish_id });
-  console.log('getDish dishDoc', dishDoc);
-  return { success: true, data: dishDoc }
-}  
+  // console.log('getDish dishDoc', dishDoc);
+  return { success: true, data: dishDoc };
+}
 
 async function getDishID(dishName) {
-  console.log(`dishName`, dishName)
+  // console.log(`dishName`, dishName)
   let dishDoc = await daoDish.findOne({ name: dishName });
-  console.log('getDishID dishDoc', dishDoc);
-  return { success: true, data: dishDoc }
-}  
+  // console.log('getDishID dishDoc', dishDoc);
+  return { success: true, data: dishDoc };
+}
 
 async function findOrCreateDish(dishName, placeId, price) {
   // find dish document
-  let dishDoc = await daoDish.findOneAndUpdate({ name: dishName, place_id: placeId }, {latest_price: price});
+  let dishDoc = await daoDish.findOneAndUpdate(
+    { name: dishName, place_id: placeId },
+    { latest_price: price }
+  );
 
   // if it doesnt exist, just create the document
   if (!dishDoc) {
-    dishDoc = await daoDish.create({ name: dishName, place_id: placeId, latest_price: price });
-  } 
+    dishDoc = await daoDish.create({
+      name: dishName,
+      place_id: placeId,
+      latest_price: price,
+    });
+  }
 
   return dishDoc;
 }
 
 async function updatePrice(dishName, placeId, price) {
-  return await daoDish.findOneAndUpdate({ name: dishName, place_id: placeId },{latest_price: price})
+  return await daoDish.findOneAndUpdate(
+    { name: dishName, place_id: placeId },
+    { latest_price: price }
+  );
 }
 
 async function updateAvgRating(dishId) {
   const reviews = await daoReview.find({ dish_id: dishId });
-  const ratings = reviews.map(review => review.rating);
-  const avgRating = ratings.length > 0 ? ratings.reduce((a, b) => a + b) / ratings.length : 0;
-  
-  return await daoDish.findByIdAndUpdate(dishId, { avg_rating: avgRating }, { new: true });
+  const ratings = reviews.map((review) => review.rating);
+  const avgRating =
+    ratings.length > 0 ? ratings.reduce((a, b) => a + b) / ratings.length : 0;
+
+  return await daoDish.findByIdAndUpdate(
+    dishId,
+    { avg_rating: avgRating },
+    { new: true }
+  );
 }
 
-async function fetchAllDishesIDs(){
+async function fetchAllDishesIDs() {
   const allDishesIDs = await daoDish.find();
   // console.log(`allDishesIDs`, allDishesIDs);
-  return allDishesIDs
+  return allDishesIDs;
 }
